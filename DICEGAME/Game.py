7dc7ae1a54784d5389df18c5262aa16c85b7a748
makzprog <makzprog@gmail.com>
2025-09-24 23:11:48 +0200

    Dice Game v1

---
+++ DICEGAME/Game.py
@@ -0,0 +1,42 @@
+import random
+
+class DiceGame:
+    def __init__(self, sides=6):
+        self.sides = sides
+    
+    # Ask user for how many dices they want to roll
+    def amount_dices(self):
+        num_dices = input("How many dices do you want to roll? ")
+        
+        if num_dices.isdigit() and int(num_dices) > 0:
+            num_dices = int(num_dices)
+            return num_dices
+        else:
+            print("Please enter a valid number.")
+            return self.amount_dices()
+    
+    # Roll the dices and display the result     
+    def roll_dices(self, num_dices):
+        count = 0
+        isActive = True
+        while isActive:
+            ask_user = input("Roll the dices? (y/n): ")
+            if ask_user == 'y' or ask_user == 'Y':
+                self.roll = tuple(random.randint(1, self.sides) for _ in range(num_dices))
+                count += 1
+                print("You rolled:", self.roll, " | Total rolls:", count)
+            elif ask_user == 'n' or ask_user == 'N':
+                isActive = False
+                print("Game Over")
+                print("Total rolls:", count)
+                break
+            else:
+                if ask_user != 'y' and ask_user != 'Y':
+                    print("Invalid input. Please enter 'y' or 'n'.")
+        return self.roll
+
+# start the program
+if __name__ == "__main__":
+    start_game = DiceGame()
+    specify_dices = start_game.amount_dices()
+    rolling_dices = start_game.roll_dices(specify_dices)
